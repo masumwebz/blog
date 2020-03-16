9263357e5b49f921229dafd6a952365ca796b5f8
Masum <masumwebz@gmail.com>
2020-03-16 22:04:15 +0600

    super user created

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Masum',
+        'title': 'Blog Post',
+        'content': 'First Content dolor sit amet, consectetg elit, sed do eiusmod tempor incididunt ut labore.',
+        'date_posted': 'Jul 3 1987',
+    },
+    {
+        'author': 'newuser',
+        'title': 'New Post',
+        'content': 'Second Content dolor sit amet, elit, sed do eiusmod tempor incididunt ut labore.',
+        'date_posted': 'aug 3 1987',
+    }
+]
+
 def home(request):
-    return render(request, 'blog/post.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog.html', context)
 
