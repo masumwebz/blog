07e13093bd476cf2504f12318a81d1923a0bfe9c
Masum <masumwebz@gmail.com>
2020-03-16 23:37:21 +0600

    Post model created & fetch data by views

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        'author': 'Masum',
-        'title': 'Blog Post',
-        'content': 'First Content dolor sit amet, consectetg elit, sed do eiusmod tempor incididunt ut labore.',
-        'date_posted': 'Jul 3 1987',
-    },
-    {
-        'author': 'newuser',
-        'title': 'New Post',
-        'content': 'Second Content dolor sit amet, elit, sed do eiusmod tempor incididunt ut labore.',
-        'date_posted': 'aug 3 1987',
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/blog.html', context)
 
