2bf7c9d54dc55846a9c06f91d265599b9300c3c7
Masum <masumwebz@gmail.com>
2020-03-19 23:54:54 +0600

    login done

--- userapp/urls.py
+++ userapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-     path('login/', views.login, name='login'),
+    path('logins/', views.loginpage, name='logins'),
     # path('logout/', views.logout, name='logout'),
   
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib import messages
-
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 
@@ -23,6 +23,20 @@ def register(request):
 
 
 
+# login page views
+def loginpage(request):
 
-def login(request):
-    return render(request,'users/register.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('blog-home')
+        else:
+            messages.info(request, 'pass ki vule gesen?')
+    context = {}
+
+    return render(request,'users/login.html',context)
