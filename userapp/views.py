69722d9f70f2cfec2610e87b9ae7536e14b61962
Masum <masumwebz@gmail.com>
2020-03-20 00:17:56 +0600

    logged hole onno page redirect kora done

--- userapp/views.py
+++ userapp/views.py
@@ -7,40 +7,47 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+#register views
 def register(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('blog-home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account successfully created for ' + user )
+                return redirect('register')
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account successfully created for ' + user )
-            return redirect('register')
+        context = { 'form':form }
+        return render(request, 'users/register.html', context)
 
-    context = { 'form':form }
-    return render(request, 'users/register.html', context)
 
 
 
 
 # login page views
 def loginpage(request):
+    if request.user.is_authenticated:
+        return redirect('blog-home')
+    else:
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('blog-home')
-        else:
-            messages.info(request, 'pass ki vule gesen?')
-    context = {}
+            if user is not None:
+                login(request, user)
+                return redirect('blog-home')
+            else:
+                messages.info(request, 'pass ki vule gesen?')
+        context = {}
 
-    return render(request,'users/login.html',context)
+        return render(request,'users/login.html',context)
 
 #logout views
 def logoutuser (request):
