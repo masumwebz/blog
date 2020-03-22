b1ee84e9cf6d2121702fa60931ecb242c12b546d
Masum <masumwebz@gmail.com>
2020-03-22 19:59:29 +0600

    user profie model blog er moddhe create hoice

--- blog/models.py
+++ blog/models.py
@@ -47,3 +47,10 @@ class Album(models.Model):
     def __str__(self):
         return self.name
 
+
+
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30)
